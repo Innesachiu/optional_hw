7caf64e14aff13ad931514739a9d4df86b1b6576
Innesachiu <innesa.ntu@gmail.com>
2026-06-13 16:22:01 +0800

    op_hw

--- CampusMarketplaceWeb/backend/src/filter/CorsFilter.java
+++ CampusMarketplaceWeb/backend/src/filter/CorsFilter.java
@@ -1,31 +1,32 @@
 package filter;
 
-import com.sun.net.httpserver.Filter;
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 
-import java.io.IOException;
-
 /**
- * Adds CORS headers so frontend pages can call backend APIs.
+ * Utility for adding CORS headers.
  */
-public class CorsFilter extends Filter {
-    @Override
-    public void doFilter(HttpExchange exchange, Chain chain) throws IOException {
-        exchange.getResponseHeaders().set("Access-Control-Allow-Origin", "*");
-        exchange.getResponseHeaders().set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        exchange.getResponseHeaders().set("Access-Control-Allow-Headers", "Content-Type, Authorization");
-        exchange.getResponseHeaders().set("Access-Control-Max-Age", "3600");
-
-        if ("OPTIONS".equalsIgnoreCase(exchange.getRequestMethod())) {
-            exchange.sendResponseHeaders(204, -1);
-            return;
-        }
-
-        chain.doFilter(exchange);
+public class CorsFilter {
+    /**
+     * Adds CORS headers to every response.
+     *
+     * @param exchange HTTP exchange
+     */
+    public static void apply(HttpExchange exchange) {
+        Headers headers = exchange.getResponseHeaders();
+        headers.set("Access-Control-Allow-Origin", "*");
+        headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        headers.set("Access-Control-Max-Age", "3600");
     }
 
-    @Override
-    public String description() {
-        return "Adds CORS headers";
+    /**
+     * Checks whether the request is a CORS preflight request.
+     *
+     * @param exchange HTTP exchange
+     * @return true if request method is OPTIONS
+     */
+    public static boolean isPreflight(HttpExchange exchange) {
+        return "OPTIONS".equalsIgnoreCase(exchange.getRequestMethod());
     }
 }
